4654aae9184bcaeea60efea3f3a386595dd50fac
MarianaJacome <49417194+MarianaJacome@users.noreply.github.com>
2021-09-15 17:01:08 -0500

    bd

--- bd/ingeniat.sql
+++ bd/ingeniat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-09-2021 a las 10:20:43
+-- Tiempo de generación: 15-09-2021 a las 23:59:46
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.4.23
 
@@ -56,8 +56,6 @@ CREATE TABLE `grupos` (
   `nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
   `pagina` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `icon` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `color_icon` varchar(100) COLLATE utf8_spanish_ci DEFAULT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT 1,
   `created` datetime DEFAULT current_timestamp(),
   `modified` datetime DEFAULT NULL
@@ -67,8 +65,8 @@ CREATE TABLE `grupos` (
 -- Volcado de datos para la tabla `grupos`
 --
 
-INSERT INTO `grupos` (`id`, `nombre`, `descripcion`, `pagina`, `icon`, `color_icon`, `activo`, `created`, `modified`) VALUES
-('8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'Administrador', 'Encargado del sistema.', '', 'bi bi-hdd', '', 1, '2021-09-14 15:15:47', '2021-09-14 16:33:35');
+INSERT INTO `grupos` (`id`, `nombre`, `descripcion`, `pagina`, `activo`, `created`, `modified`) VALUES
+('8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'Administrador', 'Encargado del sistema.', '', 1, '2021-09-14 15:15:47', '2021-09-15 14:37:43');
 
 -- --------------------------------------------------------
 
@@ -89,11 +87,11 @@ CREATE TABLE `grupos_menus` (
 INSERT INTO `grupos_menus` (`id`, `grupo_id`, `menu_id`) VALUES
 ('4685b227-47d1-4e0c-826d-b8d078d2fdc6', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '894b9b4e-9699-4413-af59-d2fc611e0b43'),
 ('4cdfeff3-7a01-4527-95e6-ec62fc67f22f', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '356c270a-7e01-422a-ad4f-95652f79588a'),
+('707d3c42-f50a-4835-8077-42bb3c1bf587', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '02241d10-b424-4041-a68b-3334150d63cb'),
 ('aa7f19cf-84a3-4b88-93b5-c1853a07c31e', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '216d99a6-4cad-4cf9-ac50-3fca7d8ecb76'),
 ('b93adddb-e8e7-4b87-a35d-e9b76cabc05c', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'e6dc709c-e544-4244-b467-b16779028fb6'),
-('c4041c45-e56a-4680-bee0-3b0227886d86', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '02241d10-b424-4041-a68b-3334150d63cb'),
-('fca8f7e6-f384-44e0-8c32-177bfac4be82', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b'),
-('fdae42bd-8857-4bd8-85cf-ee35eb15b03f', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'f11f293a-1c97-4f45-8755-5b005f604829');
+('ed047576-f3fb-48fc-8b8c-84040f69cc12', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'f11f293a-1c97-4f45-8755-5b005f604829'),
+('fca8f7e6-f384-44e0-8c32-177bfac4be82', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b');
 
 -- --------------------------------------------------------
 
@@ -137,7 +135,7 @@ CREATE TABLE `menus` (
 --
 
 INSERT INTO `menus` (`id`, `menu_id`, `icon`, `nombre`, `href`, `position`, `activo`, `created`, `modified`) VALUES
-('02241d10-b424-4041-a68b-3334150d63cb', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b', '', 'Permisos', '/permisos/index', 4, 1, '2021-09-14 19:26:58', '2021-09-15 03:17:19'),
+('02241d10-b424-4041-a68b-3334150d63cb', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b', '', 'Permisos', '/permisos/index', 4, 1, '2021-09-14 19:26:58', '2021-09-15 10:48:47'),
 ('216d99a6-4cad-4cf9-ac50-3fca7d8ecb76', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b', '', 'Roles', '/grupos/index', 3, 1, '2021-09-14 19:26:21', '2021-09-14 19:26:21'),
 ('356c270a-7e01-422a-ad4f-95652f79588a', NULL, 'fas fa-tachometer-alt', 'Dashboard', '/Dashboard/index', 1, 1, '2021-09-14 16:28:44', '2021-09-14 16:28:44'),
 ('894b9b4e-9699-4413-af59-d2fc611e0b43', '8ab9ebc6-9acd-48f4-8354-d2be0c11bb0b', 'fas fa-users', 'Usuarios', '/usuarios/index', 2, 1, '2021-09-14 19:21:44', '2021-09-14 19:21:44'),
@@ -154,7 +152,7 @@ INSERT INTO `menus` (`id`, `menu_id`, `icon`, `nombre`, `href`, `position`, `act
 CREATE TABLE `permisos` (
   `id` char(36) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
-  `descripción` text COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
   `controller` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `action` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
   `activo` tinyint(1) NOT NULL DEFAULT 1,
@@ -166,7 +164,7 @@ CREATE TABLE `permisos` (
 -- Volcado de datos para la tabla `permisos`
 --
 
-INSERT INTO `permisos` (`id`, `nombre`, `descripción`, `controller`, `action`, `activo`, `created`, `modified`) VALUES
+INSERT INTO `permisos` (`id`, `nombre`, `descripcion`, `controller`, `action`, `activo`, `created`, `modified`) VALUES
 ('abaf4e5e-1596-11ec-843f-9cb6d01e9372', 'Acceso a todos los permisos', 'Tendrá acceso a todos los permisos', 'Dashboard', 'index', 1, '2021-09-14 15:02:21', '2021-09-14 21:59:17');
 
 -- --------------------------------------------------------
@@ -181,10 +179,17 @@ CREATE TABLE `publicaciones` (
   `cat_estatu_id` char(36) COLLATE utf8_spanish_ci NOT NULL,
   `titulo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
-  `imagen` text COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` text COLLATE utf8_spanish_ci DEFAULT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `publicaciones`
+--
+
+INSERT INTO `publicaciones` (`id`, `usuario_id`, `cat_estatu_id`, `titulo`, `descripcion`, `imagen`, `created`, `modified`) VALUES
+('93a55093-cb30-4068-b712-0b78ba453135', 'fffe7621-1598-11ec-843f-9cb6d01e9372', '10714ce4-816d-4df3-8417-282d1e3565dc', 'prueba', 'prueba', NULL, '2021-09-15 16:58:04', '2021-09-15 16:58:04');
 
 -- --------------------------------------------------------
 
@@ -211,6 +216,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `grupo_id`, `nombre`, `apellido`, `correo`, `usuario`, `password`, `imagen`, `activo`, `created`, `modified`) VALUES
+('2f6ca970-4233-4f5d-918b-fa4a3897ac4d', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'Super', 'Usuario', 'superusuario@gmail.com', 'root', '$2y$10$iDPhMlqSjL1tu6tY8O1RtOZxnk/2zOw2YXZA2xjjK4YA5Oqt1NYR2', '', 1, '2021-09-15 13:00:44', '2021-09-15 13:00:44'),
 ('fffe7621-1598-11ec-843f-9cb6d01e9372', '8c42ea1d-1598-11ec-843f-9cb6d01e9372', 'Mariana Fernanda', 'Jacome Ruiz', 'marianajacome98@gmail.com', 'admin', '$2y$10$EJ.QRFNxRxDVXhgAMO47Eeg1e3cKzeI4rSey0KoyEPfqk448PlXN.', '', 1, '2021-09-14 15:19:01', '2021-09-14 22:16:30');
 
 --
